b0e6d0062784080a81cb980524f055ce604d5a9b
Mari-bekker <mshooch@gmail.com>
2022-02-26 22:50:21 -0500

    working code  without the callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,16 +4,28 @@ const request = require('request');
 
 // Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
 
-request ('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  //console.log(body);
-  //console.log(typeof body) // it returns a string
+let breed = process.argv.slice(2);
 
-
+const catRequester = function (breed) {
+  request (`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+   console.log("There was an error in the request: ", error)
+ }
+  // turning the response into an object from a string
   const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-  console.log(data[0]);
+
+  if (data[0] === undefined) {
+    console.log("requested breed not found")
+  } else {
+    console.log(data[0].description);
+  }
+  
 }
   )
 
-  //Let's use JSON.parse to convert the JSON string into an actual object.
+};
+
+catRequester(breed);
+
+
+  //Allow the user to specify the breed name using command-line arguments.
