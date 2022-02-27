f0552b6941cf0cca0b9c185e3c5fdaa329365623
Mari-bekker <mshooch@gmail.com>
2022-02-27 16:50:14 -0500

    fix refactoring of function

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,8 +2,6 @@ const request = require('request');
 
 // the api end point: https://api.thecatapi.com/v1/breeds/search?q=sib
 
-// Write the logic in breedFetcher.js to fetch the Siberian data from the API endpoint using request.
-
 const fetchBreedDescription = function(breed, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
     if (error) return callback(error, null);
@@ -17,17 +15,9 @@ const fetchBreedDescription = function(breed, callback) {
     // turning the response into an object from a string
     const data = JSON.parse(body);
     callback(null, data[0].description);
-
-    // if (data[0] === undefined) {
-    //   console.log("requested breed not found");
-    // } else {
-    //   console.log(data[0].description);
-    // }
     
   });
 
 };
-
-fetchBreedDescription(breed);
 
 module.exports = { fetchBreedDescription };
